aa8b9df16644f87c0a92cb331da1bcf7874cefb6
Nachiket <jadhao1044@gmail.com>
2023-07-23 12:10:02 +0530

    first commit

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -2,7 +2,7 @@ package com.driver;
 
 import java.util.*;
 
-public class git Gmail extends Email {
+public class Gmail extends Email {
 
     int inboxCapacity; //maximum number of mails inbox can store
     //Inbox: Stores mails. Each mail has date (Date), sender (String), message (String). It is guaranteed that message is distinct for all mails.
